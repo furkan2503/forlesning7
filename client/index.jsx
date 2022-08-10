605dc19d67c7396a2110948b14f5a3cf5be31ad6
Furkan <fantastisk-4@hotmail.com>
2022-08-10 14:43:00 +0200

    listMovies on the client side *useloader *fetchJSON

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+
 
 function FrontPage() {
     return <div>
@@ -12,9 +14,68 @@ function FrontPage() {
     </div>;
 }
 
+function useLoading(loadingFunction){
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+
+   async function load() {
+        try{
+            setLoading(true);
+            setData(await loadingFunction());
+        } catch (error) {
+            setError(error);
+        }finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        load();
+    });
+
+    return {
+        loading, error, data
+    };
+
+}
+
+
+async function fetchJSON (url) {
+    const res = await fetch(url)
+    if(res.ok) {
+        throw new Error(`Failed to load${res.status}: ${res.statusText}`)
+    }
+    return await res.json();
+}
+
+
 function ListMovies() {
+
+    const {loading, error, data} = useLoading(
+        async () => fetchJSON("/api/movies")
+    );
+
+    if(loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>
+            <h1>Error</h1>
+            <div>{error.toString()}</div>
+        </div>
+    }
+
     return <div>
         <h1>Movies in database</h1>
+
+        <ul>
+            {data.map((movie => <li key= {
+                movie.title}>{movie.title}</li>
+            ))}
+        </ul>
+
     </div>;
 }
 
